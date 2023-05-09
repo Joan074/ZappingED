4eaad95b5723a36ceb10fe453cf7d68518c660c6
cl3ar <sergio.scr43@gmail.com>
2023-05-09 16:56:14 +0200

    Initial commit

---
+++ src/Zapping.java
@@ -0,0 +1,7 @@
+public class Zapping {
+
+    public static int Canales(int canal, int destino) {
+        int clicks = canal - destino;
+        return clicks;
+    }
+}
---
+++ src/ZappingTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZappingTest {
+
+    @Test
+    void ZappingTest1(){
+        //action
+        int control = Zapping.Canales(0,0);
+        //assert
+        assertEquals(0,control);
+        System.out.println("Test Correcto");
+    }
+
+    @Test
+    void ZappingTest2(){
+        //action
+
+        //assert
+    }
+
+    @Test
+    void ZappingTest3(){
+        //action
+
+        //assert
+    }
+
+    @Test
+    void ZappingTest4(){
+        //action
+
+        //assert
+    }
+
+    @Test
+    void ZappingTest5(){
+        //action
+
+        //assert
+    }
+
+    @Test
+    void ZappingTest6(){
+        //action
+
+        //assert
+    }
+
+    @Test
+    void ZappingTest7(){
+        //action
+
+        //assert
+    }
+}
